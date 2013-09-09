0813e9a335469b6a8aa921188046b709eb4f6005
james2doyle <james2doyle@gmail.com>
2013-09-08 22:12:07 -0400

    initial plugin

--- tasks/sundown.js
+++ tasks/sundown.js
@@ -6,19 +6,56 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
+ 'use strict';
 
-module.exports = function(grunt) {
+ module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+  var rs = require('robotskirt');
 
   grunt.registerMultiTask('sundown', 'a grunt wrapper for robotskirt(sundown)', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      punctuation: '.',
-      separator: ', '
+      extensions: {
+        autolink: false,
+        fenced_code: false,
+        lax_html_blocks: false,
+        no_intra_emphasis: false,
+        space_headers: false,
+        strikethrough: false,
+        tables: false
+      },
+      render_flags: {
+        skip_html: false,
+        skip_style: false,
+        skip_images: false,
+        skip_links: false,
+        expand_tabs: false,
+        safelink: false,
+        toc: false,
+        hard_wrap: false,
+        use_xhtml: false,
+        escape: false,
+        smartypants: false
+      },
+      separator: '\n\n'
     });
+    // lets make an array of extensions to use
+    var extensions = [], render_flags = [];
+    for (var ext in options.extensions) {
+      if (options.extensions[ext]) {
+        // extensions get passed as uppercase flags starting with EXT_
+        extensions.push(rs['EXT_'+ext.toUpperCase()]);
+      }
+    }
+    // now handle the render flags
+    for (var flag in options.render_flags) {
+      if (options.render_flags[flag]) {
+        // render_flags get passed with HTML_
+        render_flags.push(rs['HTML_'+flag.toUpperCase()]);
+      }
+    }
+    // pass the extensions to the markdown parser
+    var parser = rs.Markdown.std(extensions, render_flags);
 
     // Iterate over all specified file groups.
     this.files.forEach(function(f) {
@@ -34,13 +71,10 @@ module.exports = function(grunt) {
       }).map(function(filepath) {
         // Read file source.
         return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
+      }).join(options.separator);
 
       // Write the destination file.
-      grunt.file.write(f.dest, src);
+      grunt.file.write(f.dest, parser.render(src));
 
       // Print a success message.
       grunt.log.writeln('File "' + f.dest + '" created.');
